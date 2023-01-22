1a955639f2748332cf75cd378d316b125337a28a
Nancy Heinz <morstad@us.ibm.com>
2023-01-21 20:24:41 -0600

    PhotonVision goes to target

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -44,7 +44,7 @@ public class Robot extends TimedRobot {
         // autonomous chooser on the dashboard.
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
-        CameraServer.startAutomaticCapture();
+       // CameraServer.startAutomaticCapture();
     }
 
     /**
